519739aafba1af6bc605a27fca63c09f3f3d64a9
Кулешов Сергей Львович <sergey.kuleshov@ru.rhenus.com>
2025-05-16 00:32:11 +0300

    Выполнены задачи по заданию спринта №25 - Создан новый проект FirstComposeProject для демонстрации отображения карточки контакта     средствами JetPack Compose фреймворка. - Все задачи выполнены в модуле главной активити. - Функция ContactDetails получает на входе DataClass с данными контакта и отображает данные карточки согласно предложенному макету. - Оба варианта предложенных карточек контактов выводятся в режиме Preview. - Один из вариантов карточки показывается в режиме запуска приложения.

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,9 +3,29 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
@@ -27,15 +47,89 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun ContactDetails(contact: Contact)
     {
-        // Начинаем строить карточку контакта
-        Text(contact.name)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Первым делом показываем изображение контакта
+            Box(
+                modifier = Modifier.height(64.dp),
+                contentAlignment = Alignment.Center
+            ){
+                when (contact.imageRes == null) {
+                    true -> {
+                        Icon(
+                            modifier = Modifier.size(64.dp),
+                            painter = painterResource(id = R.drawable.circle),
+                            tint = Color.LightGray,
+                            contentDescription = null
+                        )
+                        Text(
+                            text = contact.name.take(1) + contact.familyName.take(1),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                    }
+                    false -> Image(
+                        modifier = Modifier.fillMaxSize(),
+                        painter = painterResource(id = contact.imageRes),
+                        contentDescription = null
+                    )
+                }
+            }
+            // Теперь показываем имя и отчество, если есть
+            Text("${contact.name} ${contact.surname.orEmpty()}",
+                modifier = Modifier.padding(top = 12.dp),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold)
+            // Теперь у нас фамилия и отметка избранного
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(bottom = 20.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(contact.familyName,
+                    fontSize = 20.sp)
+                if (contact.isFavorite)
+                    Image(
+                        modifier = Modifier.height(20.dp).padding(start = 6.dp),
+                        painter = painterResource(android.R.drawable.star_big_on),
+                        contentDescription = null)
+            }
+            // Теперь телефон
+            InfoRow(stringResource(R.string.phone), contact.phone)
+            // Теперь адрес
+            InfoRow(stringResource(R.string.address), contact.address)
+            // Теперь E-mail
+            if (!contact.email.isNullOrEmpty())
+                InfoRow(stringResource(R.string.email), contact.email)
+        }
     }
 
-    @Preview(name = "portrait", showSystemUi = true,
+    @Composable
+    fun InfoRow(name: String, value: String) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(name,
+                modifier = Modifier.weight(0.5f).padding(horizontal = 4.dp),
+                textAlign = TextAlign.End,
+                fontSize = 16.sp)
+            Text(value,
+                modifier = Modifier.weight(0.5f).padding(horizontal = 4.dp),
+                fontSize = 16.sp)
+        }
+    }
+
+    @Preview(showSystemUi = true,
         device = "spec:width=1080px,height=2400px,dpi=420"
     )
     @Composable
-    fun ContactDetailsPreview(){
+    fun ContactDetailsPreviewLukashin(){
         ContactDetails(contact = Contact(
             name = "Евгений",
             surname = "Андреевич",
@@ -44,6 +138,20 @@ class MainActivity : ComponentActivity() {
             phone = "+7 495 495 95 95",
             address = "г. Москва, 3-я улица Строителей, д. 25, кв. 12",
             email = "ELukashin@practicum.ru"
+        ))
+    }
+
+    @Preview(showSystemUi = true,
+        device = "spec:width=1080px,height=2400px,dpi=420"
+    )
+    @Composable
+    fun ContactDetailsPreviewKuzyakin(){
+        ContactDetails(contact = Contact(
+            name = "Василий",
+            familyName = "Кузякин",
+            phone = "---",
+            address = "Ивановская область, дер. Крутово, д. 4",
+            imageRes = R.drawable.kuzyakin
         ))
     }
 
